70de7568278997cab12ddfda84e7944f818d69fe
Matias Jansson <3801893+dunder@users.noreply.github.com>
2020-03-28 14:50:50 +0100

    Solved first star 2015 day 12

---
+++ Solutions/2015/Day12.fs
@@ -0,0 +1,34 @@
+﻿module AoC.E2015.Day12
+
+open AoC.IO
+open System.Text.RegularExpressions
+
+let input = readInputText "2015" "Day12"
+
+let countNumbers input =
+    let m = Regex.Matches(input, "(-?\d+)")
+    m
+    |> Seq.map (fun m -> m.Value |> int)
+    |> Seq.sum
+
+let firstStar () =
+    
+    countNumbers input
+
+let secondStar () =
+
+    0
+
+module Tests =
+
+    open Xunit
+
+    [<Fact>]
+    let ``first star`` () =
+        
+        Assert.Equal(156366, firstStar())
+
+    [<Fact>]
+    let ``second star`` () =
+
+        Assert.Equal(96852, secondStar())
