c4d67bf28e9b69ce8b4622284773537b3fb999b8
Matias Jansson <3801893+dunder@users.noreply.github.com>
2020-04-19 17:31:47 +0200

    Solved 2015 day 14 first star

---
+++ Solutions/2015/Day14.fs
@@ -0,0 +1,100 @@
+﻿module AoC.E2015.Day14
+
+open System.Text.RegularExpressions
+
+open AoC
+open IO
+
+// --- Day 14: Reindeer Olympics ---
+
+let input = readInputLines "2015" "Day14"
+
+type Raindeer = { Name: string; Speed : int; Flight : int; Rest : int} with
+    member x.Distance = (fun seconds -> 
+        let interval = x.Flight + x.Rest
+        let fullCycles = seconds / interval
+        let remaining = seconds % interval
+        let distance = x.Speed * fullCycles * x.Flight + x.Speed * min remaining x.Flight
+        distance
+    )
+
+let (|Regex|_|) pattern input = 
+    let m = Regex.Match(input, pattern)
+
+    if m.Success
+    then Some(List.tail [for g in m.Groups -> g.Value])
+    else None
+
+let parse input = 
+    input
+    |> Seq.map (fun line -> 
+        match line with 
+        | Regex "^(.*) can fly (\d+) km/s for (\d+) seconds, but then must rest for (\d+) seconds." 
+            [name; speed; flight; rest] -> { Name = name; Speed = speed |> int; Flight = flight |> int; Rest = rest |> int }
+        | x -> failwithf "Cannot parse: %s" x
+    )
+
+let maxDistance input seconds =
+    let deers = parse input
+    
+    deers 
+    |> Seq.map (fun deer -> deer.Distance seconds)
+    |> Seq.max
+    
+
+let firstStar () =
+    
+    maxDistance input 2503
+
+let secondStar () = 
+    0
+
+
+module Tests =
+
+    open Xunit
+
+    [<Fact>]
+    let ``first star`` () =
+
+        Assert.Equal(2640, firstStar())
+
+    [<Fact>]
+    let ``second star`` () =
+
+        Assert.Equal(-1, secondStar())
+        
+    [<Theory>]
+    [<InlineData(1, 14)>]
+    [<InlineData(10, 140)>]
+    [<InlineData(11, 140)>]
+    [<InlineData(1000, 1120)>]
+    let ``distance calculation comet`` seconds expected =
+        let comet = { Name = "Comet"; Speed = 14; Flight = 10; Rest = 127 }
+
+        let distance = comet.Distance seconds 
+        
+        Assert.Equal(expected, distance)
+    
+    [<Theory>]
+    [<InlineData(1, 16)>]
+    [<InlineData(10, 160)>]
+    [<InlineData(11, 176)>]
+    [<InlineData(1000, 1056)>]
+    let ``distance calculation dancer`` seconds expected =
+
+        let dancer = { Name = "Dancer"; Speed = 16; Flight = 11; Rest = 162 }
+
+        let distance = dancer.Distance seconds
+        
+        Assert.Equal(expected, distance)
+
+    [<Fact>]
+    let ``first star example`` () =
+        let input = [|
+            "Comet can fly 14 km/s for 10 seconds, but then must rest for 127 seconds."
+            "Dancer can fly 16 km/s for 11 seconds, but then must rest for 162 seconds."
+        |]
+
+        let maxDistance = parse input |> Seq.map (fun deer -> deer.Distance 1000) |> Seq.max
+        Assert.Equal(1120, maxDistance)
