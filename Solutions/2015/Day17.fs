37b62609f86d1e79b72154a312402d3eb82220e7
Matias Jansson <3801893+dunder@users.noreply.github.com>
2020-10-31 17:52:30 +0100

    Solved 2015 day 17

---
+++ Solutions/2015/Day17.fs
@@ -0,0 +1,67 @@
+﻿module AoC.E2015.Day17
+
+open AoC
+open Combinatorics
+open IO
+
+
+// --- Day 17: No Such Thing as Too Much ---
+
+let input = readInputLines "2015" "Day17"
+ 
+let parse input = 
+    0
+
+let firstStar () =
+    let containers = input |> Seq.map int |> List.ofSeq
+    
+    let summedUp = containers |> List.sort |> List.scan (+) 0
+    let max = (summedUp |> List.findIndex (fun x -> x > 150))
+    
+    let summedUpDesc = containers |> List.sortDescending |> List.scan (+) 0
+    let min = (summedUpDesc |> List.findIndex (fun x -> x > 150))
+
+    [min..max] 
+    |> List.map (fun n -> combinations n containers)
+    |> List.collect (fun combo -> combo)
+    |> List.filter (fun combo -> combo |> List.sum = 150)
+    |> List.length
+
+
+        
+let secondStar () = 
+    let containers = input |> Seq.map int |> List.ofSeq
+    
+    let summedUp = containers |> List.sort |> List.scan (+) 0
+    let max = (summedUp |> List.findIndex (fun x -> x > 150))
+    
+    let summedUpDesc = containers |> List.sortDescending |> List.scan (+) 0
+    let min = (summedUpDesc |> List.findIndex (fun x -> x > 150))
+
+    let xs = 
+        [min..max] 
+        |> List.map (fun n -> combinations n containers)
+        |> List.collect (fun combo -> combo)
+        |> List.filter (fun combo -> combo |> List.sum = 150)
+        |> List.map (fun combo -> (List.length combo, combo))
+        |> List.sortBy (fun item -> fst item)
+
+    let min = fst xs.Head
+
+    xs 
+    |> List.filter (fun x -> fst x = min)
+    |> List.length
+
+module Tests =
+
+    open Xunit
+
+    [<Fact>]
+    let ``first star`` () =
+
+        Assert.Equal(4372, firstStar())
+
+    [<Fact>]
+    let ``second star`` () =
+
+        Assert.Equal(4, secondStar())
--- Solutions/Combinatorics.fs
+++ Solutions/Combinatorics.fs
@@ -1,14 +1,52 @@
 ﻿module Combinatorics
 
+
+// returns a list of lists of all the combinations of n of the list of l, see the examples below
 let rec combinations n l = 
     match n, l with
     | 0, _ -> [[]]
     | _, [] -> []
     | k, (x::xs) -> List.map ((@) [x]) (combinations (k-1) xs) @ combinations k xs
 
+// returns a sequence of all the permutations of the provided list, from https://stackoverflow.com/a/2184129/3047206
 let rec permutations = function
-       | []      -> seq [List.empty]
+       | [] -> seq [List.empty]
        | x :: xs -> Seq.collect (insertions x) (permutations xs)
    and insertions x = function
-       | []             -> [[x]]
+       | [] -> [[x]]
        | (y :: ys) as xs -> (x::xs)::(List.map (fun x -> y::x) (insertions x ys))
+
+module Tests =
+
+    open Xunit
+
+    [<Fact>]
+    let ``combinations example 1`` () =
+        let actual = combinations 2 [1..3]
+
+        Assert.Equal(3, List.length actual)
+
+        Assert.Contains(actual, fun combination -> combination |> Seq.length = 2 && combination |> Seq.contains 1 && combination |> Seq.contains 2)
+        Assert.Contains(actual, fun combination -> combination |> Seq.length = 2 && combination |> Seq.contains 2 && combination |> Seq.contains 3)
+        Assert.Contains(actual, fun combination -> combination |> Seq.length = 2 && combination |> Seq.contains 1 && combination |> Seq.contains 3)
+
+    [<Fact>]
+    let ``combinations example 2`` () =
+        let actual = combinations 3 [1..3]
+        
+        Assert.Equal(1, List.length actual)
+        Assert.Contains(1, actual.[0])
+        Assert.Contains(2, actual.[0])
+        Assert.Contains(3, actual.[0])
+        
+    [<Fact>]
+    let ``permutations example`` () =
+        let actual = permutations [1..3]
+            
+        Assert.Equal(6, Seq.length actual)
+        Assert.Contains([1;2;3], actual)
+        Assert.Contains([1;3;2], actual)
+        Assert.Contains([2;3;1], actual)
+        Assert.Contains([2;1;3], actual)
+        Assert.Contains([3;1;2], actual)
+        Assert.Contains([3;2;1], actual)
