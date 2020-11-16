0462bb97080f4139c03680ae31479911becb7f4d
Matias Jansson <3801893+dunder@users.noreply.github.com>
2020-11-16 20:37:45 +0100

    Solved first star on day 19 of 2015

--- Solutions/2015/Day19.fs
+++ Solutions/2015/Day19.fs
@@ -1,5 +1,7 @@
 ﻿module AoC.E2015.Day19
 
+open System.Text.RegularExpressions;
+
 open AoC
 open IO
 
@@ -7,10 +9,43 @@ open IO
 
 let input = readInputLines "2015" "Day19"
 
+type Replacement = { Match:string; Generates:string}
+
+let (|Regex|_|) pattern input = 
+    let m = Regex.Match(input, pattern)
+    
+    if m.Success 
+    then Some(List.tail [for g in m.Groups -> g.Value])
+    else None
+
+let parseReplacements (lines:seq<string>) = 
+    lines
+    |> Seq.filter (fun line -> line.Contains("=>"))
+    |> Seq.map (fun line -> 
+        match line with
+        | Regex "(.*) => (.*)" [m;g] -> { Match = m; Generates = g }
+        | _ -> failwithf "Unrecognized input %s" line
+    )
+
+let parseMolecule lines = lines |> Seq.last
+    
 
 let firstStar () =
-    0
-
+    let replacements = parseReplacements input
+    let molecule = parseMolecule input
+    
+    replacements
+    |> Seq.map (fun r -> 
+        Regex.Matches(molecule, r.Match)
+        |> Seq.map (fun m ->
+            let regEx = new Regex(r.Match)
+            regEx.Replace(molecule, r.Generates, 1, m.Index)
+        )
+    )
+    |> Seq.collect (fun x -> x)
+    |> Seq.distinct
+    |> Seq.length
+    
 let secondStar () = 
     0
 
@@ -22,7 +57,7 @@ module Tests =
     [<Fact>]
     let ``first star`` () =
 
-        Assert.Equal(-1, firstStar())
+        Assert.Equal(509, firstStar())
 
     [<Fact>]
     let ``second star`` () =
