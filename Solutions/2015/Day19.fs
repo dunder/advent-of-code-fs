9c208c28b5b35ebf9f515769854c2e0fa7b5604b
Matias Jansson <3801893+dunder@users.noreply.github.com>
2020-11-16 13:46:07 +0100

    Prepare day 19 of 2015

---
+++ Solutions/2015/Day19.fs
@@ -0,0 +1,30 @@
+﻿module AoC.E2015.Day19
+
+open AoC
+open IO
+
+// --- Day 19: Medicine for Rudolph ---
+
+let input = readInputLines "2015" "Day19"
+
+
+let firstStar () =
+    0
+
+let secondStar () = 
+    0
+
+
+module Tests =
+
+    open Xunit
+
+    [<Fact>]
+    let ``first star`` () =
+
+        Assert.Equal(-1, firstStar())
+
+    [<Fact>]
+    let ``second star`` () =
+
+        Assert.Equal(-1, secondStar())
