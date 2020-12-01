1178dbe54d934586f7276d04bc1ddfbb94de6403
Matias Jansson <3801893+dunder@users.noreply.github.com>
2020-12-01 17:47:32 +0100

    Solution for 2015 day 20

--- Solutions/2015/Day20.fs
+++ Solutions/2015/Day20.fs
@@ -7,13 +7,33 @@ open IO
 
 let input = 36000000
 
+// well had to get help this time https://www.reddit.com/r/adventofcode/comments/3xjpp2/day_20_solutions/
 
 let firstStar () =
+    
+    let houses = Array.zeroCreate input
 
-    0
+    for elf in 1..input/10 do
+        for house in elf..elf..input/10 do  
+            houses.[house] <- houses.[house] + elf*10
+
+    houses |> Seq.findIndex (fun x -> x >= input)
 
 let secondStar () = 
-    0
+    
+    let houses = Array.zeroCreate input
+
+    for elf in 1..input/10 do
+        let potentialHouses = {elf..elf..input/10}
+        let housesToVisit =
+            if Seq.length potentialHouses > 50 then
+                potentialHouses |> Seq.take 50
+            else    
+                potentialHouses
+        for house in housesToVisit do  
+            houses.[house] <- houses.[house] + elf*11
+
+    houses |> Seq.findIndex (fun x -> x >= input)
 
 
 module Tests =
@@ -22,11 +42,11 @@ module Tests =
 
     [<Fact>]
     let ``first star`` () =
-        Assert.Equal(-1, firstStar())
+        Assert.Equal(831600, firstStar())
 
     [<Fact>]
     let ``second star`` () =
-        Assert.Equal(-1, secondStar())
+        Assert.Equal(884520, secondStar())
         
 
 
