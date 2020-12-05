d4e9c29de66f02512af0dcbf91ee6e9eb2e4fd05
Matias Jansson <3801893+dunder@users.noreply.github.com>
2020-12-05 13:07:38 +0100

    Solved day 5 of 2020

--- Solutions/2020/Day05.fs
+++ Solutions/2020/Day05.fs
@@ -3,16 +3,77 @@
 open AoC
 open IO
 
-// 
+// --- Day 5: Binary Boarding ---
 
 let input = readInputLines "2020" "Day05" |> List.ofSeq
 
+let parse rows columns (spec:string) =
+    let rowSpec = spec.[0..6]
+    let columnSpec = spec.[7..9]
+
+    let row = 
+        rowSpec
+        |> Seq.fold (fun acc n ->
+            let parts = acc |> List.splitInto 2
+            match n with 
+            | 'F' -> parts |> List.head
+            | 'B' -> parts |> List.last
+            | _ -> failwithf "Unmatched specification: %s %c"  rowSpec n
+        ) [ 0..rows-1 ]
+
+    let column = 
+        columnSpec
+        |> Seq.fold (fun acc n ->
+            let parts = acc |> List.splitInto 2
+            match n with 
+            | 'L' -> parts |> List.head
+            | 'R' -> parts |> List.last
+            | _ -> failwithf "Unmatched specification: %s %c"  rowSpec n
+        ) [ 0..columns-1 ]
+    
+    (row.Head, column.Head)
+
+let seatId (row, column) =
+    row * 8 + column
 
 let firstStar () =
-    0
+    
+    input |> List.map (parse 128 8) |> List.map seatId |> List.max
 
+    
 let secondStar () = 
-    0
+    
+    let seats = input |> List.map (parse 128 8)
+
+    let seatIds = seats |> List.map (fun s -> s |> seatId) |> Set.ofList
+
+    let nonFullRows = 
+        seats 
+        |> List.groupBy fst 
+        |> Seq.filter (fun s -> s |> snd |> List.length < 8)
+
+    let emptySeats = 
+        nonFullRows
+        |> Seq.map snd
+        |> Seq.map (fun seats ->
+            let row = seats |> List.head |> fst
+            let allSeatsInARow = [0..7] |> Set.ofList
+            let occupied = seats |> List.map snd |> Set.ofList
+            let available = Set.difference allSeatsInARow occupied
+            (row, available |> Seq.exactlyOne)
+        )
+
+    let mySeat = 
+        emptySeats 
+        |> Seq.filter (fun seat -> 
+            let seatId' = seat |> seatId
+            seatIds |> Set.contains (seatId' + 1)
+            &&
+            seatIds |> Set.contains (seatId' - 1)
+        )
+        |> Seq.exactlyOne
+
+    mySeat |> seatId
 
 
 module Tests =
@@ -21,11 +82,11 @@ module Tests =
 
     [<Fact>]
     let ``first star`` () =
-        Assert.Equal(-1, firstStar())
+        Assert.Equal(855, firstStar())
 
     [<Fact>]
     let ``second star`` () =
-        Assert.Equal(-1, secondStar())
+        Assert.Equal(552, secondStar())
         
 
 
