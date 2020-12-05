b6e4c0d2983a4d8349d5a248923064c43e846a07
Matias Jansson <3801893+dunder@users.noreply.github.com>
2020-12-05 21:21:49 +0100

    Solved the last couple of puzzles of 2015

---
+++ Solutions/2015/Day24.fs
@@ -0,0 +1,58 @@
+﻿module AoC.E2015.Day24
+
+open AoC
+open IO
+open Combinatorics
+
+// --- Day 24: It Hangs in the Balance ---
+
+let input = readInputLines "2015" "Day24" |> List.ofSeq
+
+let parse lines = lines |> List.map int64
+
+let quantumEntanglement packages =
+    packages |> Seq.fold (*) 1L
+
+let idealConfiguration compartments packages = 
+    let targetWeight = (packages |> Seq.sum) / (compartments |> int64)
+    [2..input.Length] 
+    |> Seq.map (fun n -> 
+        packages |> combinations n |> Seq.filter (fun x -> x |> Seq.sum = targetWeight)
+    )
+    |> Seq.find (fun cs -> not (cs |> Seq.isEmpty))
+    |> Seq.sortBy (fun gs -> quantumEntanglement gs)
+    |> Seq.head
+    
+
+let firstStar () =
+    parse input
+    |> idealConfiguration 3
+    |> quantumEntanglement 
+
+let secondStar () = 
+    parse input
+    |> idealConfiguration 4
+    |> quantumEntanglement 
+
+
+module Tests =
+
+    open Xunit
+
+    [<Fact>]
+    let ``first star`` () =
+        Assert.Equal(11846773891L, firstStar())
+
+    [<Fact>]
+    let ``second star`` () =
+        Assert.Equal(80393059L, secondStar())
+
+    [<Fact>]
+    let ``first star example`` () =
+
+        let result =
+            [1L..5L] @ [7L..11L]
+            |> idealConfiguration 3
+            |> quantumEntanglement 
+
+        Assert.Equal(99L, result)
---
+++ Solutions/2015/Day25.fs
@@ -0,0 +1,41 @@
+﻿module AoC.E2015.Day25
+
+open AoC
+
+// --- Day 25: Let It Snow ---
+
+let row =2947
+let column = 3029
+
+//     | 1   2   3   4   5   6  
+//  ---+---+---+---+---+---+---+
+//   1 |  1   3   6  10  15  21
+//   2 |  2   5   9  14  20
+//   3 |  4   8  13  19
+//   4 |  7  12  18
+//   5 | 11  17
+//   6 | 16
+
+let codeAt row column = 
+    let atCol1 = [1..row] |> Seq.fold (fun acc n -> acc + (n-1)) 1
+    if column = 1 then
+        atCol1
+    else
+        [2..column] |> Seq.fold (fun acc n -> 
+            acc + n + row - 1
+        ) atCol1
+
+let code nr = [1..nr-1] |> Seq.fold (fun acc n -> acc * 252533L % 33554393L) 20151125L 
+
+let firstStar () =
+    
+    codeAt row column |> code
+
+
+module Tests =
+
+    open Xunit
+
+    [<Fact>]
+    let ``first star`` () =
+        Assert.Equal(-1L, firstStar())
