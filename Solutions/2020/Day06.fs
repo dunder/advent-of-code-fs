ff5d9423f3e3ed202acef055ff027cbb682c6c17
Matias Jansson <3801893+dunder@users.noreply.github.com>
2020-12-06 10:49:53 +0100

    Solved day 6 of 2020

--- Solutions/2020/Day06.fs
+++ Solutions/2020/Day06.fs
@@ -3,16 +3,47 @@
 open AoC
 open IO
 
-// 
+// --- Day 6: Custom Customs ---
 
 let input = readInputLines "2020" "Day06" |> List.ofSeq
 
+let parse lines =
+
+    lines
+    |> Seq.mapFold (fun acc line -> 
+       if line = "" then
+            ((-1,line), acc + 1)
+       else
+            ((acc,line), acc)
+    ) 0
+    |> fst
+    |> Seq.filter (fun (i, _) -> i <> -1)
+    |> Seq.groupBy (fun (i, _) -> i)
+    
+
+let count line =
+    line 
+    |> Seq.distinct 
+    |> Seq.length
+
+let count2 lines = 
+    lines 
+    |> Seq.distinct 
+    |> Seq.map Set.ofSeq 
+    |> Seq.reduce Set.intersect 
+    |> Seq.length
 
 let firstStar () =
-    0
+    parse input
+    |> Seq.map (fun x -> snd x |> Seq.map snd |> String.concat "")
+    |> Seq.map count 
+    |> Seq.reduce (+)
 
 let secondStar () = 
-    0
+    parse input 
+    |> Seq.map (fun x -> snd x |> Seq.map snd)
+    |> Seq.map count2
+    |> Seq.reduce (+)
 
 
 module Tests =
@@ -21,11 +52,11 @@ module Tests =
 
     [<Fact>]
     let ``first star`` () =
-        Assert.Equal(-1, firstStar())
+        Assert.Equal(6310, firstStar())
 
     [<Fact>]
     let ``second star`` () =
-        Assert.Equal(-1, secondStar())
+        Assert.Equal(3193, secondStar())
         
 
 
