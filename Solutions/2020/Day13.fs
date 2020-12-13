f594a0554c16b8fcd4c3b1c769b804518096b631
Matias Jansson <3801893+dunder@users.noreply.github.com>
2020-12-13 21:04:39 +0100

    Solved day 13 of 2020

--- Solutions/2020/Day13.fs
+++ Solutions/2020/Day13.fs
@@ -3,29 +3,75 @@
 open AoC
 open IO
 
-// 
+// --- Day 13: Shuttle Search ---
 
 let input = readInputLines "2020" "Day13" |> List.ofSeq
 
+let parse (lines: list<string>) = 
+    let earliest = lines.[0] |> int
+    let buses = lines.[1].Split(',') |> Seq.filter (fun x -> x <> "x") |> Seq.map int
+    earliest, buses
+
+let parse2 (lines: list<string>) = 
+    lines.[1].Split(',') 
+    |> Array.mapi (fun i s -> 
+        if s = "x" then 
+            (i |> int64, None) 
+        else 
+            (i |> int64, Some (s |> int64))
+    )
+    |> List.ofArray
 
 let firstStar () =
-    0
+    let at, buses = parse input
+    let firstDeparture = 
+        Seq.unfold (fun (time, _) -> 
+            let bus = buses |> Seq.tryFind(fun bus -> time % bus = 0)
+            
+            Some ((time, bus), (time + 1, bus))) (at, None)
+        |> Seq.find (fun (time, bus) -> 
+            match bus with
+            | Some x -> true
+            | None -> false
+        )
+    (firstDeparture |> snd).Value * ((firstDeparture |> fst) - at)
 
 let secondStar () = 
-    0
 
+    let buses = 
+        parse2 input 
+        |> List.choose (fun (t, bus) -> 
+            match bus with
+            | Some x -> Some ((t, x))
+            | None -> None
+         )
 
+    let _, bus = buses |> List.head
+
+    let result = 
+        buses.Tail 
+        |> Seq.fold (fun (time, step) (idx, bus) -> 
+            let t = 
+                Seq.unfold (fun t -> 
+                    if (t + idx) % bus = 0L then
+                        None
+                    else 
+                        Some (t + step, t + step)
+                ) time
+                |> Seq.last
+            (t, bus * step)
+        ) (0L, bus)
+
+    result |> fst
+ 
 module Tests =
 
     open Xunit
 
     [<Fact>]
     let ``first star`` () =
-        Assert.Equal(-1, firstStar())
+        Assert.Equal(153, firstStar())
 
     [<Fact>]
     let ``second star`` () =
-        Assert.Equal(-1, secondStar())
-        
-
-
+        Assert.Equal(471793476184394L, secondStar())
--- Solutions/Program.fs
+++ Solutions/Program.fs
@@ -1,21 +1,35 @@
 ﻿module Aoc.Program
 
-open AoC
 open CommandLine
+open System.IO
+open System.Reflection
+open System.Xml.Linq
 
 
 [<Verb("new", HelpText = "Create files for a new day")>]
 type NewOptions = {
-    [<Option('e', "event", Required = true, HelpText = "An event (such as 2015, 2016 etc)")>] Event : string
-    [<Option('d', "day", Required = true, HelpText = "A day (such as 1, 2 etc)")>] Day : string
+    [<Option('e', "event", Required = true, HelpText = "An event (such as 2015, 2016 etc)")>] Event : int
+    [<Option('d', "day", Required = true, HelpText = "A day (such as 1, 2 etc)")>] Day : int
+    [<Option("whatif", Required = false, HelpText = "What if, simulates without creating any files")>] WhatIf : bool
     
 }
 
 [<Verb("run", HelpText = "Run the firstStar() or secondStar() methods of a day, or both")>]
 type RunOptions = {
+    [<Option('e', "event", Required = true, HelpText = "An event (such as 2015, 2016 etc)")>] Event : int
+    [<Option('d', "day", Required = true, HelpText = "A day (such as 1, 2 etc)")>] Day : int
     [<Option('f', "first", Required = false, HelpText = "Run first star")>] First : bool
     [<Option('s', "second", Required = false, HelpText = "Run second star")>] Second : bool
 }
+
+let callFunction (event: int) (day: int) (name: string) = 
+    let asm = Assembly.GetExecutingAssembly()
+  
+    let moduleName = sprintf "AoC.E%i.Day%02i, Solutions" event day
+    let dayModules = asm.GetTypes() |> Array.filter (fun t -> t.AssemblyQualifiedName.StartsWith(moduleName))
+    let dayModule = dayModules |> Array.exactlyOne
+    let method = dayModule.GetMethod(name)
+    method.Invoke(null, [||])
 
 let printByType (text : string, result : obj) =
     match result with
@@ -24,15 +38,58 @@ let printByType (text : string, result : obj) =
     | :? System.String as x -> printf "%s %s" text x
     | x -> printf "%s %A" text result
 
-let newAocDay opts =
-    0
+let newAocDay (opts: NewOptions) =
+    let eventDirectory = Path.Combine(__SOURCE_DIRECTORY__, sprintf "%i" opts.Event)
+    let inputDirectory = Path.Combine(eventDirectory, "Input")
+    let dayFile = Path.Combine(eventDirectory, sprintf "Day%02i.fs" opts.Day)
+
+    if not <| Directory.Exists(eventDirectory) then
+        printfn "This is a new event, creating new directories ..."
+        if not <| opts.WhatIf then
+            Directory.CreateDirectory(eventDirectory) |> ignore
+        printfn "Created a new event directory: %s" eventDirectory
+        if not <| opts.WhatIf then
+            Directory.CreateDirectory(inputDirectory) |> ignore
+        printfn "Created a new input directory: %s" inputDirectory
+
+    if not <| File.Exists(dayFile) then
+        let templateFile = Path.Combine(__SOURCE_DIRECTORY__, "DayTemplate.fs")
+        let template = File.ReadAllText(templateFile)
+        let content = 
+            template
+                .Replace("DayXX", sprintf "Day%02i" opts.Day)
+                .Replace("Event20XX", sprintf "Event%i" opts.Event)
+                .Replace("20XX", sprintf "%i" opts.Event)
+        
+        if not <| opts.WhatIf then
+            File.WriteAllText(dayFile, content)
+        printfn "Created the solution file: %s" dayFile
+        
+        let inputFile = Path.Combine(inputDirectory, sprintf "Day%02i.txt" opts.Day)
+        
+        if not <| opts.WhatIf then
+            File.WriteAllText(inputFile, "")
+        printfn "Created the input file: %s" inputFile
+
+        let projectFile = XDocument.Load(Path.Combine(__SOURCE_DIRECTORY__, "Solutions.fsproj"))
+
+        let root = projectFile.Root.Elements
+
+
+        0
+    else
+        printfn "The file '%s' already exists." dayFile
+        1
 
 let runAocDay (opts: RunOptions) = 
+    
     if opts.First then
-        printByType ("First star: ", E2016.Day02.firstStar())
+        let result = callFunction opts.Event opts.Day "firstStar"
+        printByType ("First star: ", result)
         printfn "\n"
     if opts.Second then
-        printByType ("Second star:", E2016.Day02.secondStar())
+        let result = callFunction opts.Event opts.Day "secondStar"
+        printByType ("Second star:", result)
         printfn "\n"
     0
 
