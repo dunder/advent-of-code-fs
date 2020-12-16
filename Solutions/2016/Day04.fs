abcc82c94ea2c0bed38510f76a85c820b03261f8
Matias Jansson <3801893+dunder@users.noreply.github.com>
2020-12-16 16:54:00 +0100

    Solved day 4 of 2016

--- Solutions/2016/Day04.fs
+++ Solutions/2016/Day04.fs
@@ -8,7 +8,7 @@ open System.Text.RegularExpressions
 
 let input = readInputLines "2016" "Day04" |> List.ofSeq
 
-type RoomIdentifier = { name: string; id: int; checksum: string }
+type RoomIdentifier = { Name: string; Id: int; Checksum: string }
 
 
 let toRoomIdentifier line = 
@@ -18,13 +18,12 @@ let toRoomIdentifier line =
     let id = m.Groups.[2].Value |> int
     let checksum = m.Groups.[3].Value
 
-    { name = name; id = id; checksum = checksum }
+    { Name = name; Id = id; Checksum = checksum }
 
 let parse lines = lines |> List.map toRoomIdentifier
 
-
 let countLetters room = 
-    room.name 
+    room.Name 
     |> Seq.filter System.Char.IsLetter 
     |> Seq.groupBy (id)
     |> Seq.map (fun (key, values) -> (key, values |> Seq.length))
@@ -35,7 +34,6 @@ let letterCountSort (c1, count1) (c2, count2) =
     else
         (c1 |> int) - (c2 |> int)
     
-
 let isValidChecksum roomIdentifier =
     
     let checksum = 
@@ -45,16 +43,31 @@ let isValidChecksum roomIdentifier =
         |> Seq.map fst
         |> System.String.Concat
 
-    checksum = roomIdentifier.checksum
+    checksum = roomIdentifier.Checksum
+
+let alphabetLength = ('z' |> int) - ('a' |> int) + 1
+let a = 'a' |> int
+
+let shift times c = 
+    match c with
+    | '-' -> ' '
+    | c -> ((c |> int) - a + times) % alphabetLength + a |> char
+
+let decrypt (room: RoomIdentifier) = room.Name |> String.map (shift room.Id) |> System.String.Concat
 
 let firstStar () =
-    let x = parse input
-    let y = countLetters { name = "aaaaa-bbb-z-y-x-"; id = 123; checksum = "abxyz" }
-
-    0
+    parse input
+    |> List.filter isValidChecksum
+    |> List.sumBy (fun x -> x.Id)
 
 let secondStar () = 
-    0
+    let northPoleObjects =
+        parse input
+        |> List.filter isValidChecksum
+        |> List.map (fun room -> decrypt room, room)
+        |> List.find (fun (decrypted, room) -> decrypted.Contains("northpole"))
+        
+    (northPoleObjects |> snd).Id
 
 
 module Tests =
@@ -76,7 +89,25 @@ module Tests =
     [<InlineData("a-b-c-d-e-f-g-h-987[abcde]", true)>]
     [<InlineData("not-a-real-room-404[oarel]", true)>]
     [<InlineData("totally-real-room-200[decoy]", false)>]
-    let ``isValidChecksum`` (identifier: string) (valid: bool) =
+    let ``valid checksum`` (identifier: string) (valid: bool) =
         
         let isValid = isValidChecksum (toRoomIdentifier identifier)
         Assert.Equal(valid, isValid)
+
+    [<Theory>]
+    [<InlineData('a', 1, 'b')>]
+    [<InlineData('z', 1, 'a')>]
+    let ``shift`` (c: char) (times: int) (expected: char) =
+        
+        let shifted = shift times c
+
+        Assert.Equal(expected, shifted)
+
+    [<Fact>]
+    let ``decrypt`` () =
+        
+        let room = toRoomIdentifier "qzmt-zixmtkozy-ivhz-343[zimth]"
+
+        let decrypted = decrypt room
+
+        Assert.Equal("very encrypted name ", decrypted)
