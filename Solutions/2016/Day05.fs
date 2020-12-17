1498a4cfd365767aed0c728c6cd8275163ad780b
Matias Jansson <3801893+dunder@users.noreply.github.com>
2020-12-17 17:55:50 +0100

    Solved day 17 of 2020

---
+++ Solutions/2016/Day05.fs
@@ -0,0 +1,102 @@
+﻿module AoC.E2016.Day05
+
+open AoC
+open Cryptography
+
+// --- Day 5: How About a Nice Game of Chess? ---
+
+let input = "reyedfim"
+
+let hash index (input: string) =
+    md5 (input + (index |> string))
+
+let generate (startIndex: int) (input: string) =
+    Seq.unfold (fun (i, (p: string)) -> 
+        if p.StartsWith("00000") then
+            None
+        else
+            let next = hash i input
+            Some ((i, next), (i + 1, next))
+    ) (startIndex, "")
+    |> Seq.last
+
+let toLower (s: string) = s |> String.map System.Char.ToLower
+
+let password (input: string) =
+    Seq.unfold (fun (i, (pw: string)) ->
+        if pw.Length = 8 then
+            None
+        else
+            let idx, c = generate i input
+            let current = pw + (c.[5] |> string)
+            Some ((i, current), (idx + 1, current))
+                
+    ) (0, "")
+    |> Seq.last
+    |> snd
+    |> toLower
+
+let password2 (input: string) =
+    Seq.unfold (fun (i, pw: list<int*char>) ->
+        if pw.Length = 8 then
+            None
+        else
+            let idx, c = generate i input
+
+            let currentCharIdx = c.[5]
+            let currentChar = c.[6]
+
+            let next = 
+                if System.Char.IsDigit(currentCharIdx) then
+                    let num = currentCharIdx |> string |> int
+                    if num < 8 then
+                        (num, currentChar)::pw
+                    else
+                        pw
+                else
+                    pw
+
+            Some ((i, next), (idx + 1, next))
+                
+    ) (0, [])
+    |> Seq.last
+    |> snd
+    |> List.sortBy fst
+    |> Seq.map snd
+    |> System.String.Concat
+    |> toLower
+
+let firstStar () =
+    password input
+
+let secondStar () = 
+    ""
+
+
+module Tests =
+
+    open Xunit
+
+    [<Fact>]
+    let ``first star`` () =
+
+        Assert.Equal("f97c354d", firstStar())
+
+    [<Fact>]
+    let ``second star`` () =
+
+        Assert.Equal("863dde27", secondStar())
+
+    [<Fact>]
+    let ``first star example`` () =
+
+        let generatedPassword = password "abc"
+
+        Assert.Equal("18f47a30", generatedPassword)
+
+    [<Fact>]
+    let ``second star example`` () =
+
+        let generatedPassword = password2 "abc"
+
+        Assert.Equal("05ace8e3", generatedPassword)
--- Solutions/2020/Day17.fs
+++ Solutions/2020/Day17.fs
@@ -2,17 +2,218 @@
 
 open AoC
 open IO
+open System.Collections.Generic
 
-// 
+// --- Day 17: Conway Cubes ---
 
 let input = readInputLines "2020" "Day17" |> List.ofSeq
 
+type Point3D = { X: int; Y: int; Z: int}
+type Point4D = { X: int; Y: int; Z: int; W: int}
+
+type Cube3D = Set<Point3D>
+type Cube4D = Set<Point4D>
+
+module Point3D = 
+    let neighbors (p: Point3D) =
+        [
+            for z in p.Z-1..p.Z+1 do
+                for y in p.Y-1..p.Y+1 do
+                    for x in p.X-1..p.X+1 do
+                        let neighbor = { X = x; Y = y; Z = z }
+                        if neighbor <> p then
+                            yield neighbor
+        ]
+        
+module Point4D = 
+
+    let neighbors p =
+        [
+            for w in p.W-1..p.W+1 do
+                for z in p.Z-1..p.Z+1 do
+                    for y in p.Y-1..p.Y+1 do
+                        for x in p.X-1..p.X+1 do
+                            let neighbor = { X = x; Y = y; Z = z; W = w}
+                            if neighbor <> p then
+                                yield neighbor
+        ]
+
+module Cube3D =
+    
+    let parse (lines: list<string>) = 
+        let rows = Seq.length lines
+        let columns = String.length <| Seq.head lines
+        let cube =
+            [
+                for column in 0..columns-1 do
+                    for row in 0..rows-1 do
+                        let c = lines.[row].[column]
+                        if c = '#' then
+                            yield { X = column; Y = row; Z = 0 }
+            ]
+            |> Set.ofSeq
+                
+        cube
+
+    let xmin (cube: Cube3D) = cube |> Seq.map (fun p -> p.X) |> Seq.min
+    let xmax (cube: Cube3D) = cube |> Seq.map (fun p -> p.X) |> Seq.max
+
+    let ymin (cube: Cube3D) = cube |> Seq.map (fun p -> p.Y) |> Seq.min
+    let ymax (cube: Cube3D) = cube |> Seq.map (fun p -> p.Y) |> Seq.max
+
+    let zmin (cube: Cube3D) = cube |> Seq.map (fun p -> p.Z) |> Seq.min
+    let zmax (cube: Cube3D) = cube |> Seq.map (fun p -> p.Z) |> Seq.max
+
+    let bounds (cube: Cube3D) = (xmin cube, xmax cube), (ymin cube, ymax cube), (zmin cube, zmax cube)
+
+    let activate p (cube: Cube3D) = cube |> Set.add p
+    let inactivate p (cube: Cube3D) = cube |> Set.remove p
+
+    let active p (cube: Cube3D) = cube |> Set.contains p
+
+    let space (cube: Cube3D) = 
+        [
+            let (xmin, xmax), (ymin, ymax),(zmin, zmax) = bounds cube
+
+            for z in zmin..zmax do
+                for y in ymin..ymax do
+                    for x in xmin..xmax do
+                        let p = { X = x; Y = y; Z = z }
+                        yield p
+                        yield! p |> Point3D.neighbors
+        ]
+
+    let activeNeighbors p (cube: Cube3D) = Point3D.neighbors p |> Seq.filter (fun n -> cube |> active n)
+
+    let oneCycle (cube: Cube3D): Cube3D =
+
+        cube
+        |> space
+        |> Seq.map (fun p -> 
+            let count = 
+                cube 
+                |> activeNeighbors p
+                |> Seq.length
+                
+            if cube |> active p then 
+                if count = 2 || count = 3 then
+                    Some(p)
+                else 
+                    None
+            else 
+                if count = 3 then
+                    Some(p)
+                else
+                    None
+        )
+        |> Seq.choose (id)
+        |> Set.ofSeq
+        
+    let cycle (cube: Cube3D) =
+        [1..6]
+        |> Seq.fold (fun cube i -> 
+            cube |> oneCycle
+        ) cube
+
+
+
+module Cube4D =
+    
+    let parse (lines: list<string>) = 
+        let rows = Seq.length lines
+        let columns = String.length <| Seq.head lines
+        let cube =
+            [
+                for column in 0..columns-1 do
+                    for row in 0..rows-1 do
+                        let c = lines.[row].[column]
+                        if c = '#' then
+                            yield { X = column; Y = row; Z = 0; W = 0 }
+            ]
+            |> Set.ofSeq
+                
+        cube
+
+    let xmin (cube: Cube4D) = cube |> Seq.map (fun p -> p.X) |> Seq.min
+    let xmax (cube: Cube4D) = cube |> Seq.map (fun p -> p.X) |> Seq.max
+
+    let ymin (cube: Cube4D) = cube |> Seq.map (fun p -> p.Y) |> Seq.min
+    let ymax (cube: Cube4D) = cube |> Seq.map (fun p -> p.Y) |> Seq.max
+
+    let zmin (cube: Cube4D) = cube |> Seq.map (fun p -> p.Z) |> Seq.min
+    let zmax (cube: Cube4D) = cube |> Seq.map (fun p -> p.Z) |> Seq.max
+
+    let wmin (cube: Cube4D) = cube |> Seq.map (fun p -> p.W) |> Seq.min
+    let wmax (cube: Cube4D) = cube |> Seq.map (fun p -> p.W) |> Seq.max
+
+    let bounds (cube: Cube4D) = (xmin cube, xmax cube), (ymin cube, ymax cube), (zmin cube, zmax cube), (wmin cube, wmax cube)
+
+    let activate p (cube: Cube4D) = cube |> Set.add p
+    let inactivate p (cube: Cube4D) = cube |> Set.remove p
+
+    let active p (cube: Cube4D) = cube |> Set.contains p
+
+    let space (cube: Cube4D) = 
+        [
+            let (xmin, xmax), (ymin, ymax), (zmin, zmax), (wmin, wmax) = bounds cube
+
+            for w in wmin..wmax do
+                for z in zmin..zmax do
+                    for y in ymin..ymax do
+                        for x in xmin..xmax do
+                            let p = { X = x; Y = y; Z = z; W = w}
+                            yield p
+                            yield! p |> Point4D.neighbors
+        ]
+
+    let neighborLookup (cube: Cube4D) = 
+        cube 
+        |> space
+        |> Seq.map (fun p -> (p, p |> Point4D.neighbors))
+        |> dict
+
+    let activeNeighbors p (cube: Cube4D) = p |> Point4D.neighbors |> Seq.filter (fun n -> cube |> active n)
+
+    let oneCycle (cube: Cube4D): Cube4D =
+        cube
+        |> space
+        |> Seq.map (fun p -> 
+            let count = 
+                cube 
+                |> activeNeighbors p
+                |> Seq.length
+                
+            if cube |> active p then 
+                if count = 2 || count = 3 then
+                    Some(p)
+                else 
+                    None
+            else 
+                if count = 3 then
+                    Some(p)
+                else
+                    None
+        )
+        |> Seq.choose (id)
+        |> Set.ofSeq
+        
+    let cycle (cube: Cube4D) =
+        [1..6]
+        |> Seq.fold (fun (cube) i -> 
+            cube |> oneCycle 
+        ) cube
 
 let firstStar () =
-    0
+
+    Cube3D.parse input
+    |> Cube3D.cycle 
+    |> Seq.length
 
 let secondStar () = 
-    0
+
+    Cube4D.parse input
+    |> Cube4D.cycle 
+    |> Seq.length
 
 
 module Tests =
@@ -21,11 +222,23 @@ module Tests =
 
     [<Fact>]
     let ``first star`` () =
-        Assert.Equal(-1, firstStar())
+        Assert.Equal(368, firstStar())
 
     [<Fact>]
     let ``second star`` () =
-        Assert.Equal(-1, secondStar())
-        
+        Assert.Equal(2696, secondStar())
 
+    [<Fact>]
+    let ``first star example`` () =
 
+        let input = [
+            ".#."
+            "..#"
+            "###"
+        ]
+
+        let cube = Cube3D.parse input
+
+        let cycled = Cube3D.cycle cube |> Seq.length
+
+        Assert.Equal(112, cycled)
