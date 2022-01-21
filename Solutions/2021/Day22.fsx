861943e22a58138321e1122f313a888c5f34ec02
Matias Jansson <3801893+dunder@users.noreply.github.com>
2022-01-21 17:12:20 +0100

    Solved day 22 of 2021

--- Solutions/2021/Day22.fsx
+++ Solutions/2021/Day22.fsx
@@ -36,6 +36,7 @@ let example2 = [
     "off x=18..30,y=-20..-8,z=-3..13"
     "on x=-41..9,y=-7..43,z=-33..15"
     "on x=-54112..-39298,y=-85059..-49293,z=-27449..7877"
+    "on x=967..23432,y=45373..81175,z=27513..53682"
 ]
 
 type Cuboid = { XRange: int*int; YRange: int*int; ZRange: int*int }
@@ -175,60 +176,332 @@ let example3 = [
     "off x=-93533..-4276,y=-16170..68771,z=-104985..-24507"
 ]
 
+module Range = 
 
-let sortRanges range1 range2 = 
-    if fst range1 > fst range2 then
-        range2, range1
-    else 
-        range1, range2
+    let sort range1 range2 = 
+        if fst range1 > fst range2 then
+            range2, range1
+        else 
+            range1, range2
 
-let rangeIntersects range1 range2 =
-    let range1, range2 = sortRanges range1 range2
+    let intersects range1 range2 =
+        let range1, range2 = sort range1 range2
 
-    let start1, stop1 = range1
-    let start2, stop2 = range2
+        let start1, stop1 = range1
+        let start2, stop2 = range2
 
-    start2 >= start1 && start2 <= stop1
+        start2 >= start1 && start2 <= stop1
 
-let intersects cuboid1 cuboid2 =
 
-    let xIntersect = rangeIntersects cuboid1.XRange cuboid2.XRange
-    let yIntersect = rangeIntersects cuboid1.YRange cuboid2.YRange
-    let zIntersect = rangeIntersects cuboid1.ZRange cuboid2.ZRange
-    xIntersect && yIntersect && zIntersect
+    let intersection range1 range2 =
 
-let rangeIntersection range1 range2 =
-    let range1, range2 = sortRanges range1 range2
+        if intersects range1 range2 then
+            let range1, range2 = sort range1 range2
 
-    let intersectionStart = fst range2
-    let intersectionEnd = snd range1
+            let intersectionStart = fst range2
+            let intersectionEnd = min (snd range1) (snd range2)
 
-    intersectionStart, intersectionEnd
+            Some (intersectionStart, intersectionEnd)
+        else 
+            None
 
-let intersection cuboid1 cuboid2 = 
-    
-    let xIntersection = rangeIntersection cuboid1.XRange cuboid2.XRange
-    let yIntersection = rangeIntersection cuboid1.YRange cuboid2.YRange
-    let zIntersection = rangeIntersection cuboid1.ZRange cuboid2.ZRange
+    // true if range totally includes rangeMaybeIncluded
+    // note: order dependent
+    let includes rangeMaybeIncluded range =
+        if intersects range rangeMaybeIncluded then
+            let rStart, rEnd = range
+            let candidateStart, candidateEnd = rangeMaybeIncluded
+            rStart <= candidateStart && rEnd >= candidateEnd
+        else 
+            false
 
-    { XRange = xIntersection; YRange = yIntersection; ZRange = zIntersection }
-    
+    // return a list of the intersection ranges of range2 with range1
+    // intersections on "the outside" from range1 (think of range1 as
+    // the solid part)
+    // note that the order of arguments matter for this function
+    let intersections range1 range2 = 
+        // printfn "intersections: range1 = %O, range2 = %O" range1 range2
+        match range1, range2 with
+        // r1:     |-|
+        // r2:   |-----|
+        //       |-|-|-|
+        | (r1start, r1end), (r2start, r2end)  when r2start < r1start && r2end > r1end -> 
+            // printfn "case 1: %O" [(r2start, r1start-1); (r1start, r1end); (r1end+1, r2end)]
+            [(r2start, r1start-1); (r1start, r1end); (r1end+1, r2end)]
+        // r1:   |-|
+        // r2:   |-----|
+        //       |-|---|
+        | (r1start, r1end), (r2start, r2end)  when r2start = r1start && r2end > r1end -> 
+            // printfn "case 2: %O" [(r1start, r1end); (r1end+1, r2end)]
+            [(r1start, r1end); (r1end+1, r2end)]
+        // r1:       |-|
+        // r2:   |-----|
+        | (r1start, r1end), (r2start, r2end)  when r2start < r1start && r2end = r1end -> 
+            // printfn "case 3: %O" [(r2start, r1start-1); (r1start, r2end)]
+            [(r2start, r1start-1); (r1start, r2end)]
+        // r1:   |-----|
+        // r2:       |---|
+        | (r1start, r1end), (r2start, r2end)  when r2start > r1start && r2end > r1end -> 
+            // printfn "case 4: %O" [(r2start, r1end); (r1end+1, r2end)]
+            [(r2start, r1end); (r1end+1, r2end)]
+        // r1:   |-----|
+        // r2: |---|
+        | (r1start, r1end), (r2start, r2end)  when r2start < r1start && r2end < r1end -> 
+            // printfn "case 5: %O" [(r2start, r1start-1); (r1start, r2end)]
+            [(r2start, r1start-1); (r1start, r2end)]
+        // r1:  |-----|    |-----|    |-----|
+        // r2:    |---|     |---|     |---|    
+        | _ -> 
+            // printfn "default case"
+            [range2]
+
+    let length range = abs (snd range - fst range + 1) |> int64
+
+// -19         33
+//  |------//---|
+//     |---//-----|
+//    -14         35
+
+let testRange1 = (-14, 35)
+let testRange2 = (-19, 33)
+
+Range.intersections testRange1 testRange2
+
+let rangeIncludeChecks = [    
+    Range.includes (2,4) (1,5) = true
+    Range.includes (1,5) (1,5) = true
+    Range.includes (2,5) (1,5) = true
+    Range.includes (1,4) (1,5) = true
+    Range.includes (1,5) (2,4) = false
+    Range.includes (2,6) (1,5) = false
+    Range.includes (0,5) (1,5) = false
+    Range.includes (1,6) (1,5) = false
+    Range.includes (6,8) (1,5) = false
+]
+
+rangeIncludeChecks |> List.forall id
+
+let r1 = 1,4
+let rCover = 0,5
+let coverIntersection = Range.intersection r1 rCover
+let rCovered = 2,3 
+let coveredIntersection = Range.intersection r1 rCovered
+let uncovered = 5,6
+let uncoveredIntersection = Range.intersection r1 uncovered
+Range.intersection rCover r1 = Range.intersection r1 rCover
+Range.intersection rCovered r1 = Range.intersection r1 rCovered
+
+let r4 = 2,3
+let r5 = 1,4
+Range.intersections r5 r4
+
+module Cuboid =
+
+    let intersects cuboid1 cuboid2 =
+
+        let xIntersect = Range.intersects cuboid1.XRange cuboid2.XRange
+        let yIntersect = Range.intersects cuboid1.YRange cuboid2.YRange
+        let zIntersect = Range.intersects cuboid1.ZRange cuboid2.ZRange
+
+        xIntersect && yIntersect && zIntersect
+
+    let intersection cuboid1 cuboid2 = 
+        
+        let xIntersection = Range.intersection cuboid1.XRange cuboid2.XRange
+        let yIntersection = Range.intersection cuboid1.YRange cuboid2.YRange
+        let zIntersection = Range.intersection cuboid1.ZRange cuboid2.ZRange
+
+        match xIntersection, yIntersection, zIntersection with
+        | Some xIntersection, Some yIntersection, Some zIntersection -> Some { XRange = xIntersection; YRange = yIntersection; ZRange = zIntersection }
+        | _ -> None
+
+    // true if cuboid1 totally includes cuboid2
+    // note that order of the arguments matter
+    let includes cuboidMaybeIncluded cuboid = 
+        if intersects cuboid cuboidMaybeIncluded then
+            let xRangeIncluded = cuboid.XRange |> Range.includes cuboidMaybeIncluded.XRange
+            let yRangeIncluded = cuboid.YRange |> Range.includes cuboidMaybeIncluded.YRange
+            let zRangeIncluded = cuboid.ZRange |> Range.includes cuboidMaybeIncluded.ZRange
+            xRangeIncluded && yRangeIncluded && zRangeIncluded
+        else 
+            false
+
+    // split range2 according to the intersection points with range1
+    let intersectionSplit cuboid1 cuboid2 = 
+        if intersects cuboid1 cuboid2 then
+            let xIntersections = Range.intersections cuboid1.XRange cuboid2.XRange
+            let yIntersections = Range.intersections cuboid1.YRange cuboid2.YRange
+            let zIntersections = Range.intersections cuboid1.ZRange cuboid2.ZRange
+            seq {
+                for xRange in xIntersections do
+                    for yRange in yIntersections do
+                        for zRange in zIntersections do
+                            yield { XRange = xRange; YRange = yRange; ZRange = zRange }
+            }
+            |> Seq.toList
+        else 
+            [cuboid2]
+        
+    // splits cuboid2 into cuboids based on the intersections with cuboid1 and keeps the
+    // cuboids that does not intersect with cuboid1
+    let split cuboid1 cuboid2 =
+        intersectionSplit cuboid1 cuboid2
+        |> List.filter (fun cuboid -> not <| intersects cuboid cuboid1)
+
+    let count cuboid = Range.length cuboid.XRange * Range.length cuboid.YRange * Range.length cuboid.ZRange
+
+
+let c1 = { XRange = 0,4; YRange = 0,4; ZRange = 0,4 }
+let c2 = { XRange = 1,5; YRange = 2,6; ZRange = 1,3 }
+
+Cuboid.intersection c1 c2
+
+let cuboidLarge = { XRange = 0,4; YRange = 0,4; ZRange = 0,4 }
+let cuboidIncluded = { XRange = 1,3; YRange = 1,3; ZRange = 1,3 }
+
+cuboidLarge.XRange |> Range.includes cuboidIncluded.XRange
+cuboidLarge.YRange |> Range.includes cuboidIncluded.YRange
+cuboidLarge.ZRange |> Range.includes cuboidIncluded.ZRange
+
+
+cuboidLarge |> Cuboid.intersects cuboidIncluded
+cuboidLarge |> Cuboid.includes cuboidIncluded
+
+let cuboid1 = { XRange = 0,4; YRange = 0,4; ZRange = 0,4 }
+let cuboid2 = { XRange = 1,5; YRange = 2,6; ZRange = 1,3 }
+
+Range.intersections cuboid1.XRange cuboid2.XRange
+Range.intersections cuboid1.YRange cuboid2.YRange
+Range.intersections cuboid1.ZRange cuboid2.ZRange
+
+let intersections = Cuboid.intersectionSplit cuboid1 cuboid2
+
+let overlap = intersections |> List.filter (fun cuboid -> not <| Cuboid.intersects cuboid cuboid1)
+
+cuboid2 |> Cuboid.split cuboid1
+
+
+// turnedOn is a list of non overlapping cuboids that are already turned on
+// recursion?
+
+let turnOn turnedOn cuboid =
+    // printfn ">>>>>>>>> turnOn:"
+    // printfn ">>>>>>>>> turnedOn: %O" turnedOn
+    let turnedOnIntersects = turnedOn |> List.filter (Cuboid.intersects cuboid)
+    let newTurnedOn =
+        turnedOnIntersects
+        |> List.fold (fun parts intersectingCuboid -> 
+            // printfn ">>> intersectingCuboid: %O" intersectingCuboid
+            // printfn ">>> parts: %O" parts
+            let result = 
+                parts
+                |> List.fold (fun currentParts cuboidPart ->
+                    // printfn "currentParts: %O" currentParts
+                    // printfn "split %O on %O" cuboidPart intersectingCuboid
+                    let newParts = cuboidPart |> Cuboid.split intersectingCuboid |> Set.ofList
+                    
+                    let result = currentParts |> Set.remove cuboidPart |> Set.union newParts
+                    // printfn "result: %O" result
+                    result
+                ) (parts |> Set.ofList)
+            // printfn "new state is: %O" result
+            result |> Set.toList
+        ) [cuboid]
+
+    // printfn ">>>>>>>>> turnOn <<<<<<<<<"
+    // printfn ">>>>>>>>> result: %O" (turnedOn @ newTurnedOn)
+    turnedOn @ newTurnedOn
+
+let turnOff turnedOn cuboid = 
+    let newTurnedOn =
+        turnedOn
+        |> List.fold (fun stillTurnedOn intersectingCuboid -> 
+            let stillOn = intersectingCuboid |> Cuboid.split cuboid
+                
+            stillTurnedOn @ stillOn
+        ) []
+    newTurnedOn
+
+let cube1 = { XRange = 0,3; YRange = 0,2; ZRange = 0,0 }
+let cube2 = { XRange = 1,2; YRange = 2,3; ZRange = 0,0 }
+let cube3 = { XRange = 2,4; YRange = 2,4; ZRange = 0,0 }
+let cube4 = { XRange = 0,8; YRange = 0,8; ZRange = 0,0 }
+
+let after1 = turnOn [cube1] cube2
+let after2 = turnOn after1 cube3
+let after3 = turnOn after2 cube4
+after3 |> List.sumBy Cuboid.count
+
+
+// on x=10..12,y=10..12,z=10..12
+// on x=11..13,y=11..13,z=11..13
+
+let cuboidX = { XRange = 10,12; YRange = 10,12; ZRange = 10,12 }
+let cuboidY = { XRange = 11,13; YRange = 11,13; ZRange = 11,13 }
+let cuboidZ = { XRange = 9,11; YRange = 9,11; ZRange = 9,11 }
+
+let xUnionY = cuboidY |> Cuboid.split cuboidX
+
+
+
+let turnedOn = turnOn [cuboidX] cuboidY
+
+turnedOn |> List.sumBy Cuboid.count
+
+let result = turnOff turnedOn cuboidZ
+
+result |> List.sumBy Cuboid.count
+
+turnOn result { XRange = 10,10; YRange = 10,10; ZRange = 10,10 } |> List.sumBy Cuboid.count
+
+
+let init turnedOn rebootStep =
+    let turnOnOperation, cuboid = rebootStep
+    if turnOnOperation then
+        turnOn turnedOn cuboid 
+    else
+        turnOff turnedOn cuboid
+
+let initAll rebootSteps =
+    rebootSteps
+    |> Seq.fold init []
+
+let exampleSteps = parse example3
+let firstStarExampleCount = 
+    applyAll exampleSteps
+    |> Seq.length
+
+// on x=-5..47,y=-31..22,z=-19..33
+// on x=-44..5,y=-27..21,z=-14..35
+
+// range1 
+//        -5       47
+//        |--------|
+//  -44      5
+//  |---/----|
+
+//  | (r1start, r1end), (r2start, r2end)  when r2start < r1start && r2end < r1end -> 
+//             printfn "case 5: %O" [(r2start, r1end-1); (r1end, r2end)]
+//             [(r2start, r1end-1); (r1end, r2end)]
+
+Range.intersections (-5, 47) (-44, 5)
+
+initAll exampleSteps |> List.sumBy Cuboid.count
+
+let example3Steps = parse example3
+initAll example3Steps |> List.sumBy Cuboid.count
+
+
+let xx = exampleSteps |> List.map snd
+
+xx.[1] |> Cuboid.intersectionSplit xx.[0]
+
+let zIntersections = Range.intersections xx.[1].ZRange xx.[0].ZRange
+
 let secondStar = 
-    let rebootSteps = parse example3 |> Seq.map snd |> List.ofSeq
-
-    intersection rebootSteps.[0] rebootSteps.[1]
-    // rebootSteps
-    // |> Seq.map (fun rebootStep ->
-    //     let intersectsWith =
-    //         rebootSteps
-    //         |> Seq.filter (fun other -> other <> rebootStep)
-    //         |> Seq.filter(fun other ->
-    //             intersects other rebootStep
-    //         )
-    //         |> Seq.toList
-    //     rebootStep, intersectsWith
-    // )
-    // |> Seq.toList
+    parse input
+    |> initAll
+    |> List.sumBy Cuboid.count
 
 secondStar
 
