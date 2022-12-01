59039c99eb8f5d98441c74d265bdcd2beb77b696
Matias Jansson <3801893+dunder@users.noreply.github.com>
2022-12-01 06:26:43 +0100

    Solved day 1 of 2022

--- Solutions/2022/Day01.fs
+++ Solutions/2022/Day01.fs
@@ -1,6 +1,6 @@
 module AoC.E2022.Day01
 
-// >>> insert day tagline here <<<
+// --- Day 1: Calorie Counting ---
 
 open AoC
 open IO
@@ -8,9 +8,34 @@ open IO
 
 let input = readInputLines "2022" "Day01" |> List.ofSeq
 
-
 let firstStar () =
-    0
+    // add a blank line at the end so that last elf is recognized
+    input @ [""]
+    |> List.fold (fun (elf, total) line -> 
+        match line with 
+        | "" -> 
+            if elf > total then
+                0, elf
+            else 
+                0, total
+        | line -> 
+            let calories = line |> int
+            elf + calories, total
+    ) (0, 0)
+    |> snd
 
 let secondStar () = 
-    0
+    // add a blank line at the end so that last elf is recognized
+    input @ [""]
+    |> List.fold (fun (elf, total) line -> 
+        match line with 
+        | "" -> 
+            0, elf::total
+        | line -> 
+            let calories = line |> int
+            elf + calories, total
+    ) (0, [])
+    |> snd
+    |> List.sortDescending
+    |> List.take 3
+    |> List.sum
--- Solutions/2022/Day01.fsx
+++ Solutions/2022/Day01.fsx
@@ -1,13 +1,58 @@
-// >>> insert day tagline here <<<
+// --- Day 1: Calorie Counting ---
 
 open System.IO
 
 System.Environment.CurrentDirectory <- __SOURCE_DIRECTORY__
 
-let input = File.ReadAllLines(@".\Input\Day01.txt")
+let input = File.ReadAllLines(@".\Input\Day01.txt") |> Array.toList
 
 let parse (lines: string array) = 
     lines
+
+let example = [
+    "1000"
+    "2000"
+    "3000"
+    ""
+    "4000"
+    ""
+    "5000"
+    "6000"
+    ""
+    "7000"
+    "8000"
+    "9000"
+    ""
+    "10000"
+    "" // <-- hack to make last count
+]
+
+input
+|> List.fold (fun (elf, total) line -> 
+    match line with 
+    | "" -> 
+        if elf > total then
+            0, elf
+        else 
+            0, total
+    | line -> 
+        let calories = line |> int
+        elf + calories, total
+) (0, 0)
+
+input
+|> List.fold (fun (elf, total) line -> 
+    match line with 
+    | "" -> 
+        0, elf::total
+    | line -> 
+        let calories = line |> int
+        elf + calories, total
+) (0, [])
+|> snd
+|> List.sortDescending
+|> List.take 3
+|> List.sum
 
 let firstStar =    
     0
