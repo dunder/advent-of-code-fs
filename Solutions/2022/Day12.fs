9eb6bee8b24162461c9ff82be605a82911a99d92
Matias Jansson <3801893+dunder@users.noreply.github.com>
2022-12-18 10:44:51 +0100

    Solved day 12 of 2022

--- Solutions/2022/Day12.fs
+++ Solutions/2022/Day12.fs
@@ -9,8 +9,87 @@ open IO
 let input = readInputLines "2022" "Day12" |> List.ofSeq
 
 
-let firstStar () =
-    0
+let parse (input: string list) =
+    let data = 
+        input
+        |> Seq.map (fun line -> line |> Seq.toList)
+        |> Seq.toList
 
-let secondStar () = 
-    0
+    let rows = input.Length
+    let columns = input.Head.Length
+
+    Array2D.init rows columns (fun x y -> data[x][y])
+
+let allElements (a: 'a[,]) =
+    seq { 
+        for row in 0 .. a.GetLength(0)-1 do
+          for column in 0 .. a.GetLength(1)-1 do 
+            yield (row, column), a[row,column] 
+    }
+
+let findPositionsOf (map: char[,]) c = map |> allElements |> Seq.filter (fun (_, value) -> value = c)
+
+let findStart (map: char[,]) = findPositionsOf map 'S' |> Seq.exactlyOne |> fst
+let findEnd (map: char[,]) = findPositionsOf map 'E' |> Seq.exactlyOne |> fst
+
+let updateSourceAndTargetHeights (map: char[,]) =
+    let startRow, startColumn = findStart map
+    let endRow, endColumn = findEnd map
+    map[startRow, startColumn] <- 'a'
+    map[endRow, endColumn] <- 'z'
+    map
+
+let adjacent (map: char[,]) (node: int*int) =
+    let row, column = node
+    let width = map.GetLength(1)
+    let height = map.GetLength(0)
+    [
+        if row > 0 then yield row-1, column
+        if column < width-1 then yield row, column+1
+        if row < height-1 then yield row+1, column
+        if column > 0 then yield row, column-1
+    ]
+
+let neighbours (map: char[,]) (node: int*int) =
+    let row, column = node
+    let sourceHeight = map[row, column]
+
+    let mustClimb (targetRow, targetColumn) = 
+        let targetHeight = map[targetRow, targetColumn]
+        (targetHeight |> int) - (sourceHeight |> int) > 1
+        
+    adjacent map node
+    |> List.filter (mustClimb >> not)
+
+let goal = input |> parse |> findEnd |> Set.singleton
+let map = input |> parse |> updateSourceAndTargetHeights
+let start = input |> parse |> findStart |> Set.singleton
+    
+type NodeSet<'node when 'node : comparison> = Set<'node>
+
+let shortestPath neighbours startExploring destination = 
+
+    let neighboursNotVisited (visited: NodeSet<'node>) nodeSet =
+        neighbours nodeSet
+        |> List.filter(visited.Contains >> not) 
+        |> Set
+
+    Seq.unfold(fun ((exploring, visited): NodeSet<'node>*NodeSet<'node>) -> 
+            if visited.IsSupersetOf destination then 
+                None 
+            else
+                let newExploring = exploring |> Seq.map (neighboursNotVisited visited) |> Set.unionMany
+                let newVisited = visited + newExploring
+                let newState = newExploring, newVisited
+                Some(newState, newState)) (startExploring, startExploring)
+    |> Seq.length
+
+
+let firstStar () = shortestPath (neighbours map) start goal
+
+let secondStar () =
+    let scenicStart = 
+        findPositionsOf map 'a'
+        |> Seq.map fst 
+        |> Set
+    shortestPath (neighbours map) scenicStart goal
--- Solutions/2022/Day12.fsx
+++ Solutions/2022/Day12.fsx
@@ -15,8 +15,6 @@ let example =
         "abdefghi"
     ]
 
-let inline charToInt c = int c - int '0'
-
 let parse (input: string list) =
     let data = 
         input
@@ -30,7 +28,6 @@ let parse (input: string list) =
 
 example |> parse
 
-
 let allElements (a: 'a[,]) =
     seq { 
         for row in 0 .. a.GetLength(0)-1 do
@@ -38,11 +35,10 @@ let allElements (a: 'a[,]) =
             yield (row, column), a[row,column] 
     }
 
-let findPositionOf c (a: char[,]) =
-    a |> allElements |> Seq.find (fun (_, value) -> value = c)
+let findPositionOf (map: char[,]) c = map |> allElements |> Seq.find (fun (_, value) -> value = c)
 
-let findStart (a: char[,]) = findPositionOf 'S' a
-let findEnd (a: char[,]) = findPositionOf 'E' a
+let findStart (map: char[,]) = findPositionOf map 'S'
+let findEnd (map: char[,]) = findPositionOf map 'E'
 
 let updateSourceAndTargetHeights (map: char[,]) =
     let startRow, startColumn = findStart map |> fst
@@ -54,157 +50,77 @@ let updateSourceAndTargetHeights (map: char[,]) =
 example |> parse |> findStart
 example |> parse |> findEnd
 
-let within (row, column) (map: char[,]) =
+let adjacent (map: char[,]) (node: int*int) =
+    let row, column = node
     let width = map.GetLength(1)
     let height = map.GetLength(0)
-    row >= 0 && row <= height-1 &&
-    column >= 0 && column <= width-1
-
-let neigbours (map: char[,]) (row, column) =
-    let width = map.GetLength(1)
-    let height = map.GetLength(0)
-    seq {
+    [
         if row > 0 then yield row-1, column
         if column < width-1 then yield row, column+1
         if row < height-1 then yield row+1, column
         if column > 0 then yield row, column-1
-    }
+    ]
 
-let noClimbingNeighbours (map: char[,]) (row, column) =
+let neighbours (map: char[,]) (node: int*int) =
+    let row, column = node
     let sourceHeight = map[row, column]
 
     let mustClimb (targetRow, targetColumn) = 
         let targetHeight = map[targetRow, targetColumn]
         (targetHeight |> int) - (sourceHeight |> int) > 1
         
-    neigbours map (row, column)
-    |> Seq.filter (mustClimb >> not)
+    adjacent map node
+    |> List.filter (mustClimb >> not)
+    // |> List.filter(visited.Contains >> not)
+    // |> Set
 
-type queue<'a> =
-    | Queue of 'a list * 'a list
-
-module Queue =
-    let empty = Queue([], [])
-
-    let enqueue q e = 
-        match q with
-        | Queue(fs, bs) -> Queue(e :: fs, bs)
-
-    let dequeue q = 
-        match q with
-        | Queue([], []) -> failwith "Empty queue!"
-        | Queue(fs, b :: bs) -> b, Queue(fs, bs)
-        | Queue(fs, []) -> 
-            let bs = List.rev fs
-            bs.Head, Queue([], bs.Tail)
-
-
-type Path<'node> = 'node seq
-
-module Algorithm =
-
-    type Config<'node when 'node : comparison> =
-        {
-            neighbours : 'node -> 'node seq
-            isGoal : 'node -> bool
-        }
-
-    type private Runtime<'node when 'node : comparison> =
-        {
-            neighbours : 'node -> 'node seq
-            isGoal : 'node -> bool
-            openNodes : queue<'node>
-            visitedNodes : Set<'node>
-            cameFrom : Map<'node,'node>
-        }
-
-    let private initRuntime (start : 'node) (config : Config<'node>) =
-        {
-            neighbours = config.neighbours
-            isGoal = config.isGoal
-            openNodes =  Queue.enqueue Queue.empty start
-            visitedNodes = Set.empty
-            cameFrom = Map.empty
-        }
-
-    let rec private reconstructPath' (acc : 'node list) (toNode : 'node) (runtime : Runtime<'node>) =
-        match runtime.cameFrom.TryFind toNode with
-        | None -> toNode :: acc
-        | Some parent -> reconstructPath' (toNode :: acc) parent runtime
-
-
-    let private reconstructPath (toNode : 'node) (runtime : Runtime<'node>) =
-        reconstructPath' [] toNode runtime |> Seq.ofList
-
-    let private processChild (node : 'node) (runtime : Runtime<'node>) (child : 'node) =
-        { runtime with
-            openNodes = Queue.enqueue runtime.openNodes child
-            cameFrom = runtime.cameFrom.Add (child, node)
-        }
-
-    let rec private runAlgorithm (runtime : Runtime<'node>) =
-        let current, openNodes = Queue.dequeue runtime.openNodes
-        if runtime.isGoal current then
-            runtime |> reconstructPath current
-        else
-            let open' = openNodes
-            let visited' = runtime.visitedNodes |> Set.add current
-            let runtime' = { runtime with openNodes = open'; visitedNodes = visited' }
-            let children =
-                runtime.neighbours current
-                |> Seq.filter (visited'.Contains >> not)
-            let runtime'' =
-                children
-                |> Seq.fold (processChild current) runtime'
-            runAlgorithm runtime''
-
-    let bfs (start : 'node) (config : Config<'node>) =
-        config
-        |> initRuntime start
-        |> runAlgorithm
 
 let exampleMap = example |> parse |> updateSourceAndTargetHeights
 
-noClimbingNeighbours exampleMap (0,0)
+neighbours exampleMap (0,0)
 
 
-let exampleGoal = example |> parse |> findEnd |> fst
+let exampleGoal = example |> parse |> findEnd |> fst |> Set.singleton
+let exampleStart = example |> parse |> findStart |> fst |> Set.singleton
 
-let isExampleGoal (row, column) = (row, column) = exampleGoal
-
-let exampleBfsConfig: Algorithm.Config<int*int> = 
-    {
-        neighbours = noClimbingNeighbours exampleMap
-        isGoal = isExampleGoal
-    }
-
-let exampleStart = example |> parse |> findStart |> fst
-Algorithm.bfs exampleStart exampleBfsConfig |> Seq.length
-
-
-let goal = input |> parse |> findEnd |> fst
+let goal = input |> parse |> findEnd |> fst |> Set.singleton
 let map = input |> parse |> updateSourceAndTargetHeights
-let isGoal (row, column) = (row, column) = goal
+let start = input |> parse |> findStart |> fst |> Set.singleton
+    
+type NodeSet<'node when 'node : comparison> = Set<'node>
 
+let shortestPath neighbours startExploring destination = 
 
-let bfsConfig: Algorithm.Config<int*int> = 
-    {
-        neighbours = noClimbingNeighbours map
-        isGoal = isGoal
-    }
+    let neighboursNotVisited (visited: NodeSet<'node>) nodeSet =
+        neighbours nodeSet
+        |> List.filter(visited.Contains >> not) 
+        |> Set
 
-let start = input |> parse |> findStart |> fst
-Algorithm.bfs start bfsConfig |> Seq.length
+    Seq.unfold(fun ((exploring, visited): NodeSet<'node>*NodeSet<'node>) -> 
+            if visited.IsSupersetOf destination then 
+                None 
+            else
+                let newExploring = exploring |> Seq.map (neighboursNotVisited visited) |> Set.unionMany
+                let newVisited = visited + newExploring
+                let newState = newExploring, newVisited
+                Some(newState, newState)) (startExploring, startExploring)
+    |> Seq.length
 
+let exampleResult = shortestPath (neighbours exampleMap) exampleStart exampleGoal
 
-
-let firstStar =
-    0
+let firstStar = shortestPath (neighbours map) start goal
 
 firstStar
 
+let exampleScenicStart = exampleMap |> allElements |> Seq.filter (fun ((row, column), c) -> c = 'a') |> Seq.map fst |> Set
+
+let exampleScenicPath = shortestPath (neighbours exampleMap) exampleScenicStart exampleGoal
+
+
 let secondStar = 
-    0
+    let scenicStart = map |> allElements |> Seq.filter (fun (_, c) -> c = 'a' ) |> Seq.map fst |> Set
+    shortestPath (neighbours map) scenicStart goal
+    
 
 secondStar
 
