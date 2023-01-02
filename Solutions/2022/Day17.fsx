0c09d856067c640bc2fabeaa805dc8722c51c4c9
Matias Jansson <3801893+dunder@users.noreply.github.com>
2023-01-02 14:42:31 +0100

    Updated solution for first part of 2022 day 17

--- Solutions/2022/Day17.fsx
+++ Solutions/2022/Day17.fsx
@@ -8,242 +8,208 @@ let input = File.ReadAllText(@".\Input\Day17.txt")
 
 let example = ">>><<><>><<<>><>>><<<>>><<<><<<>><>><<>>"
 
-
 type Shape = HorizontalPipe | Plus | Angle | VerticalPipe | Square
 let shapeOrder = [ HorizontalPipe; Plus; Angle; VerticalPipe; Square ]
 
-type RockState = { Rocks: Set<int*int>; Position: int*int; AtRest: bool; JetIndex: int; Count: int }
+type Rock = { Shape: Shape; X: int; Y: int; Content: int list }
+    with 
+    member this.Height = this.Content.Length
+    member this.Collides y pixel = this.Overlaps y && (this.Layer y &&& pixel = pixel)
+    member this.Overlaps y =
+        let index = this.Index y
+        index >= 0 && index < this.Height
+    member this.Layer y = 
+        if this.Overlaps y then
+            this.Content[this.Index y]
+        else
+            failwithf "No such layer y: %i. (Height = %i)" y this.Height
+    member this.Index y = this.Y - y
+        
 
-module RockState =
-    let shape state = shapeOrder[(state.Count-1) % shapeOrder.Length]
+type CaveState = { Rocks: list<int>; FallingRock: Rock; AtRest: bool; JetIndex: int; Count: int }
 
 let chamberWidth = 7
-let rocksReleased = 2022
+let walls = 256 + 1
 let rockReleaseHeight = 3
+let bottom = 256 + 128 + 64 + 32 + 16 + 8 + 4 + 2 + 1
 
-let shapeWidth shape =
-    match shape with
-    | HorizontalPipe -> 4
-    | Plus -> 3
-    | Angle -> 3
-    | VerticalPipe -> 1
-    | Square -> 2
+shapeOrder[5 % shapeOrder.Length]
+module CaveState =
+    
+    let release y count =
+        let shape = shapeOrder[count % shapeOrder.Length]
+        
+        let content = 
+            match shape with
+            | HorizontalPipe -> [ 60 ]
+            | Plus -> [ 16; 56; 16 ]
+            | Angle -> [ 8; 8; 56 ]
+            | VerticalPipe -> [ 32; 32; 32; 32 ]
+            | Square -> [ 48; 48 ]
 
-let shapeHeight shape =
-    match shape with
-    | HorizontalPipe -> 1
-    | Plus -> 3
-    | Angle -> 3
-    | VerticalPipe -> 4
-    | Square -> 2
+        { Shape = shape; X = 0; Y = y + rockReleaseHeight + content.Length; Content = content }
 
-let bottom = [1..chamberWidth] |> List.mapi (fun i _ -> i, 0) |> Set.ofList
+    let toRest cave = 
+        let rock = cave.FallingRock
+        let y1 = cave.Rocks.Length - 1 - rock.Y
+        let y2 = y1 + rock.Height - 1
+        let before = cave.Rocks |> List.take y1
+        let newRocks = 
+            cave.Rocks[y1..y2] 
+            |> List.zip rock.Content
+            |> List.map (fun (rock, rest) -> rock ||| rest)
 
-let firstRelease = 
-    let releasePosition = 2, shapeHeight HorizontalPipe + rockReleaseHeight
-    { Rocks = bottom;  Position = releasePosition; AtRest = false; JetIndex = 0; Count = 1; }
+        let after = cave.Rocks |> List.skip (y2 + 1)
 
-let collisionPointsLeft state = 
-    let shape = RockState.shape state
-    let x, y = state.Position
-    match shape with
-    | HorizontalPipe -> [x-1, y] |> Set.ofList
-    | Plus -> [x, y; x-1, y-1; x, y-2] |> Set.ofList
-    | Angle -> [x+1, y; x+1, y-1; x-1, y-2] |> Set.ofList
-    | VerticalPipe -> [x-1, y; x-1, y-1; x-1, y-2; x-1, y-3] |> Set.ofList
-    | Square -> [x-1, y; x-1, y-1] |> Set.ofList
+        { cave with Rocks = before @ newRocks @ after; AtRest = true }
 
-let collisionPointsRight state = 
-    let shape = RockState.shape state
-    let x, y = state.Position
-    match shape with
-    | HorizontalPipe -> [x+4, y] |> Set.ofList
-    | Plus -> [x+2, y; x+3, y-1; x+2, y-2] |> Set.ofList
-    | Angle -> [x+3, y; x+3, y-1; x+3, y-2] |> Set.ofList
-    | VerticalPipe -> [x+1, y; x+1, y-1; x+1, y-2; x+1, y-3] |> Set.ofList
-    | Square -> [x+2, y; x+2, y-1] |> Set.ofList
+    let height cave = 
+        let index =
+            cave.Rocks 
+            |> List.findIndex (fun layer -> layer ||| walls <> walls) 
+        cave.Rocks.Length - index - 1
 
-let pushLeft state = 
-    let x, y = state.Position
-    let newX = 
-        if x = 0 then 
-            0 
+    let loadNextRock cave =
+        let newFallingRock = release (height cave) (cave.Count + 1)
+        let rockHeight = newFallingRock.Height
+        let fillCount = newFallingRock.Y - cave.Rocks.Length
+        let newRocks = [1..rockHeight] |> List.fold (fun state i -> walls::state) cave.Rocks
+        { cave with Rocks = newRocks; FallingRock = newFallingRock; Count = cave.Count + 1; AtRest = false }
+
+    let isCollision rock cave = 
+        cave.Rocks
+        |> List.skip (cave.Rocks.Length - 1 - rock.Y)
+        |> List.take (rock.Content |> List.length)
+        |> List.zip rock.Content 
+        |> List.map (fun (rock, rest) -> rock &&& rest)
+        |> List.exists ((<>) 0)
+
+    let push transform cave = 
+        let newContent = cave.FallingRock.Content |> List.map transform
+        let newRock = { cave.FallingRock with Content = newContent }
+
+        if cave |> isCollision newRock then
+            cave
+        else 
+            let movedRock = { cave.FallingRock with Content = newContent }
+            { cave with FallingRock = movedRock }
+
+    let pushLeft cave = push (fun r -> r <<< 1) cave
+    let pushRight cave = push (fun r -> r >>> 1) cave
+
+    let fall cave =
+        let newRock = { cave.FallingRock with Y = cave.FallingRock.Y - 1 }
+        if cave |> isCollision newRock then
+            cave |> toRest
         else
-            let collisionPonits = collisionPointsLeft state
-            if collisionPonits |> Set.intersect state.Rocks |> Set.isEmpty then
-                x-1
-            else 
-                x
-    { state with Position = newX, y }
+            { cave with FallingRock = newRock }
 
-let pushRight state =
-    let x, y = state.Position
-    let shape = RockState.shape state
-    let width = shapeWidth shape
-    let stop = chamberWidth - width
-    let newX = 
-        if x = stop then 
-            stop 
+    let rockDrop (jetPattern: string) cave =
+        Seq.unfold (fun cave ->
+            let c = jetPattern[(cave.JetIndex)%jetPattern.Length]
+            let fallenState = 
+                match c with
+                | '<' -> pushLeft cave
+                | '>' -> pushRight cave
+                | _ -> failwithf "Flaky jet push: %c" c
+                |> fall
+
+            let nextState = { fallenState with JetIndex = cave.JetIndex + 1 }
+            Some(nextState, nextState)
+        ) cave
+
+    let rockDropToRest (jetPattern: string) cave =
+        cave
+        |> rockDrop jetPattern
+        |> Seq.find (fun cave -> cave.AtRest)
+
+    let dropUntilCount count jetPattern =
+        let firstRock = release 0 0
+        let initialCave = 
+            { 
+                Rocks = [walls;walls;walls;walls;walls;walls;walls;bottom]
+                FallingRock = firstRock
+                AtRest = false
+                JetIndex = 0
+                Count = 0
+            }
+
+        Seq.unfold (fun state -> 
+            let dropState = rockDropToRest jetPattern state
+            let nextRelease = loadNextRock dropState
+            if nextRelease.Count > count then
+                None
+            else
+                Some(nextRelease, nextRelease)
+        ) initialCave
+
+let firstRock = CaveState.release 0 0
+let initialCave = { Rocks = [walls;walls;walls;walls;walls;walls;walls;bottom]; FallingRock = firstRock; AtRest = false; JetIndex = 0; Count = 0 }
+
+CaveState.pushLeft initialCave
+
+CaveState.height initialCave
+let exampleRock = CaveState.release 0 3
+
+exampleRock.Index 4
+exampleRock.Overlaps 3
+exampleRock.Overlaps 4
+exampleRock.Overlaps 5
+exampleRock
+exampleRock.Layer 4
+exampleRock.Collides 4 128
+exampleRock.Height
+
+let printChamber fromHeight toHeight state =    
+    for y = toHeight downto fromHeight do
+        if y = 0 then
+            printfn "+-------+"
         else
-            let collisionPonits = collisionPointsRight state
-            if collisionPonits |> Set.intersect state.Rocks |> Set.isEmpty then
-                x+1
-            else
-                x
-    { state with Position = newX, y }
+            let layer = state.Rocks[state.Rocks.Length-1-y]
+            printf "|"
+            for x = chamberWidth downto 1 do
+                let pixel = pown 2 x
+                if state.FallingRock.Collides y pixel then
+                    printf "@"
+                else if layer &&& pixel = pixel then
+                    printf "#"
+                else
+                    printf "."
+            printfn "|"
 
+printChamber 0 6 initialCave
 
-firstRelease |> pushRight |> pushRight |> pushRight
-firstRelease |> pushLeft |> pushLeft |> pushLeft |> pushLeft
+let newExample = 
+    initialCave 
+    |> CaveState.pushRight
+    |> CaveState.pushRight
+    |> CaveState.pushRight
+    |> CaveState.pushRight
+    |> CaveState.pushLeft
+    |> CaveState.fall
+    |> CaveState.fall
+    |> CaveState.fall
+    |> CaveState.fall
+    |> CaveState.loadNextRock
 
-
-let collisionPointsDown state =
-    let shape = RockState.shape state
-    let x, y = state.Position
-    match shape with
-    | HorizontalPipe -> [x, y-1; x+1, y-1; x+2, y-1; x+3, y-1 ] |> Set.ofList
-    | Plus -> [x, y-2; x+1, y-3; x+2, y-2] |> Set.ofList
-    | Angle -> [x, y-3; x+1, y-3; x+2, y-3] |> Set.ofList
-    | VerticalPipe -> [x, y-4] |> Set.ofList
-    | Square -> [x, y-2; x+1, y-2] |> Set.ofList
-
-let rockPoints state =
-    let shape = RockState.shape state
-    let x, y = state.Position
-    match shape with
-    | HorizontalPipe -> [x, y; x+1, y; x+2, y; x+3, y ] |> Set.ofList
-    | Plus -> [x, y-1; x+1, y; x+1, y-1; x+1, y-2; x+2, y-1] |> Set.ofList
-    | Angle -> [x, y-2; x+1, y-2; x+2, y; x+2, y-1; x+2, y-2] |> Set.ofList
-    | VerticalPipe -> [x, y; x, y-1; x, y-2; x, y-3] |> Set.ofList
-    | Square -> [x, y; x, y-1; x+1, y; x+1, y-1] |> Set.ofList 
-
-let toRest state =
-    let points = rockPoints state
-    { state with Rocks = state.Rocks |> Set.union points; AtRest = true }
-
-let rockDrop (jetPattern: string) state =
-    // printfn ">dropping a %A: %A" (RockState.shape state) state
-    Seq.unfold (fun state ->
-        let c = jetPattern[(state.JetIndex)%jetPattern.Length]
-        let pushedState = 
-            match c with
-            | '<' -> pushLeft state
-            | '>' -> pushRight state
-            | _ -> failwithf "Flaky jet push: %c" c
-
-        let x, y = pushedState.Position
-       
-        let collisionPoints = collisionPointsDown pushedState
-
-        let collisionUpdated =
-            if Set.intersect collisionPoints state.Rocks |> Set.isEmpty then
-                { pushedState with Position = x, y - 1 }
-            else
-                pushedState |> toRest
-
-        let nextState = { collisionUpdated with JetIndex = state.JetIndex + 1 }
-        Some(nextState, nextState)
-    ) state
-
-let rockDropToNth (jetPattern: string) nth state =
-    state
-    |> rockDrop jetPattern
-    |> Seq.take nth
+let newExampleCave = 
+    CaveState.dropUntilCount 2 example
+    |> Seq.last
+    |> CaveState.rockDrop example
+    |> Seq.take 1
     |> Seq.last
 
-let rockDropToRest (jetPattern: string) state =
-    state
-    |> rockDrop jetPattern
-    |> Seq.find (fun state -> state.AtRest)
+let check = CaveState.dropUntilCount 2022 example |> Seq.last
 
-let calculateHeight state =
-    state.Rocks |> Set.ofSeq |> Seq.map snd |> Seq.max
+printChamber 0 (CaveState.height check + 3 + (check.FallingRock.Height)) check
 
-let loadNextRock state = 
-    state 
-    |> calculateHeight 
-    |> fun y -> 
-        let resetState = { state with Count = state.Count + 1; AtRest = false }
-        let shapeHeight = shapeHeight (RockState.shape resetState)
-        let releasePosition = 2, y + rockReleaseHeight + shapeHeight
-        { resetState with Position = releasePosition; AtRest = false }
-
-let dropUntilCount jetPattern =
-    let initialState = firstRelease
-    Seq.unfold (fun state -> 
-        let dropState = rockDropToRest jetPattern state
-        let nextRelease = loadNextRock dropState
-        if nextRelease.Count > rocksReleased + 1 then
-            None
-        else
-            Some(nextRelease, nextRelease)
-    ) initialState
-
-bottom
-
-shapeHeight Square
-
-collisionPointsDown { Rocks = bottom; Position = (2, 1); AtRest = false; JetIndex = 0; Count = 1 }
-
-rockDropToRest example firstRelease
-|> calculateHeight
-
-rockDropToRest example { firstRelease with Count = 2; Position = 2, 4 }
-|> calculateHeight
-
-let printChamber fromHeight toHeight state =
-    for y = toHeight downto fromHeight do
-        for x = -1 to chamberWidth do
-            let position = x, y
-            let fallingRockPositions = 
-                if state.AtRest then 
-                    Set.empty
-                else    
-                    state |> rockPoints
-            if x = -1 || x = chamberWidth then
-                if y = 0 then printf "+" else printf "|"
-            else if y = 0 then
-                printf "-"
-            else if fallingRockPositions |> Set.contains position then
-                printf "@"
-            else if state.Rocks |> Set.contains position then
-                printf "#"
-            else
-                printf "."
-        printfn ""
-
-let nextRock = firstRelease |> rockDropToRest example |> loadNextRock
-
-firstRelease 
-|> rockDropToNth example 4
-|> loadNextRock
-|> printChamber 0 20
-
-dropUntilCount example 
-|> Seq.take 100
-|> Seq.last
-|> printChamber 0 200
-|> calculateHeight
-
-dropUntilCount input 
-|> Seq.last
-|> calculateHeight
-
-
-dropUntilCount input
-|> Seq.last
-|> printChamber 2900 3100
-|> calculateHeight
-
-
-firstRelease
-|> rockDropToRest example
-|> loadNextRock
-//|> rockDropToNth example 1
-|> printChamber 0 20
+check |> CaveState.height
 
 let firstStar =
-    0
+    input
+    |> CaveState.dropUntilCount 2022
+    |> Seq.last
+    |> CaveState.height
 
 firstStar
 
@@ -251,4 +217,3 @@ let secondStar =
     0
 
 secondStar
-
